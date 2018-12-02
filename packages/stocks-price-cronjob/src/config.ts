3b000c6e02910ce7a8cb28063e32672f5f23171f
Pedro Arantes <arantespp@gmail.com>
2018-12-02 19:07:05 -0200

    Stock Cronjob working

---
+++ packages/stocks-price-cronjob/src/config.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+export default {
+  AlphaVantageApiKey: '35HJRMLHL4YFT6E7',
+  IdentityPoolId: 'us-east-1:d3a1dacf-a856-4f5b-b323-63e3bddac573',
+  TableName: 'my-finances-stocks-price',
+  region: 'us-east-1',
+};
