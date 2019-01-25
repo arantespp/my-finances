ec18544925228d6cf91b7743c630126363288d29
Pedro Arantes <arantespp@gmail.com>
2019-01-25 21:01:46 -0200

    Facebook and Google login working

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -6,9 +6,15 @@ import { withAuthenticator } from 'aws-amplify-react';
 
 import asyncComponent from '@components/AsyncComponent';
 
+import config from '@config';
+
 import './App.scss';
 
 const Layout = asyncComponent(() => import('@components/Layout').then(module => module.default));
+
+const { google_client_id, facebook_app_id } = config;
+
+const federated = { google_client_id, facebook_app_id };
 
 class App extends React.Component {
   render() {
@@ -20,4 +26,4 @@ class App extends React.Component {
   }
 }
 
-export default withAuthenticator(App, false);
+export default withAuthenticator(App, true, [], federated, {});
--- packages/web/src/typings.d.ts
+++ packages/web/src/typings.d.ts
@@ -1,6 +1,14 @@
 /** @format */
 
-declare module 'aws-amplify-react';
+declare module 'aws-amplify-react' {
+  export function withAuthenticator(
+    App: React.ComponentClass,
+    hide?: boolean,
+    componentes?: any[],
+    federated?: {},
+    myTheme?: {},
+  ): React.ComponentClass;
+}
 declare module '@aws-amplify/ui';
 
 // Used by aws-appsync
